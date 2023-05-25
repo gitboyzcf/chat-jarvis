c2eef8c45c4da00a7e05a849798bbb9f61557b55
zhangchenfei <boyzcf@qq.com>
2023-05-25 17:15:12 +0800

    fix(custom): 修复

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+console.log(123)
 </script>
-更高的分数官方
+
 <template>
   <header>
-    
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
